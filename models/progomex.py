dd08d51249c4d63e2e98839e4b7c69aa138d6749
jeffrepo <jeffer.leo93@gmail.com>
2025-04-21 15:56:50 -0600

    Update

--- models/progomex.py
+++ models/progomex.py
@@ -18,3 +18,21 @@ class ProgomexGastosAdministrativo(models.Model):
     sequence = fields.Integer(string="Sequence", default=10)
     name = fields.Char("Nombre")
     cuenta_ids = fields.Many2many("account.account",string="Cuenta")
+
+class ProgomexInventario(models.Model):
+    _name = 'progomex.inventario'
+    _order = 'sequence'
+    
+    sequence = fields.Integer(string="Sequence", default=10)
+    name = fields.Char("Nombre")
+    cuenta_ids = fields.Many2many("account.account",string="Cuenta")
+    codigo = fields.Char("Codigo")
+    tipo = fields.Selection([
+        ('inicial_deudor', 'Saldo inicial deudor'),
+        ('inicia_acreedor', 'Saldo inicial acreedor'),
+        ('acumulado_deudor', 'Acumulado deudor'),
+        ('acumulado_acreedor', 'Acumulado acreedor'),
+        ('balanza_final_deudor', 'Balanza final deudor'),
+        ('balanza_final_acreedor', 'Balanza final acreedor'),
+    ], string="Tipo")
+    codigo_py = fields.Text("Codigo python")
--- wizard/reporte_estado_proyectado.py
+++ wizard/reporte_estado_proyectado.py
@@ -1,10 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
 import logging
 import xlsxwriter
 import base64
 import io
+import json
 
 class ReporteEstadoProyectado(models.TransientModel):
     _name = 'progomex.reporte_estado_proyectado'
@@ -28,7 +30,7 @@ class ReporteEstadoProyectado(models.TransientModel):
                     gastos_indirectos_opciones[cuenta_gasto.id] = gasto.name
                 lista_gastos_ids += gasto.cuenta_ids.ids
 
-            linea_analitica_ids = self.env["account.analytic.line"].search([("date",">=",fecha_inicio),("date","<=",fecha_fin),("account_id","=",cuenta_analitica_id.id),("general_account_id","in",lista_gastos_ids)])
+            linea_analitica_ids = self.env["account.analytic.line"].search([("date",">=",fecha_inicio),("date","<=",fecha_fin),("x_plan2_id","=",cuenta_analitica_id.id),("general_account_id","in",lista_gastos_ids)])
             if linea_analitica_ids:
                 for linea in linea_analitica_ids:
                     gasto = False
@@ -41,6 +43,50 @@ class ReporteEstadoProyectado(models.TransientModel):
                         gastos_indirectos[gasto] += monto
                         total += monto
         return [gastos_indirectos, total]
+
+    def obtener_inventario(self, fecha_inicio, fecha_fin):
+        datos_inventario = {}
+        apartado_inventario_id = self.env['progomex.inventario'].search([("id",">", 0)])
+        logging.warning(apartado_inventario_id)
+        if apartado_inventario_id:
+            for apartado in apartado_inventario_id:
+                nombre_apartado = apartado.codigo
+                if nombre_apartado not in datos_inventario:
+                    datos_inventario[nombre_apartado] = {"nombre": apartado.name, "total": 0}
+
+                movimiento = False
+                    
+                if apartado.tipo == "inicial_deudor":
+                    movimientos = self.env["account.move.line"].search([("account_id","in", apartado.cuenta_ids.ids),("date","<",fecha_inicio)])
+                    for m in movimientos:
+                        datos_inventario[nombre_apartado]["total"] += (m.debit - m.credit)
+                elif apartado.tipo == "inicia_acreedor":
+                    movimientos = self.env["account.move.line"].search([("account_id","in", apartado.cuenta_ids.ids),("date","<",fecha_inicio)])
+                    for m in movimientos:
+                        datos_inventario[nombre_apartado]["total"] += (m.credito - m.debit)
+                elif apartado.tipo == "acumulado_deudor":
+                    movimientos = self.env["account.move.line"].search([("account_id","in", apartado.cuenta_ids.ids),("date",">=", fecha_inicio),("date","<=", fecha_fin)])
+                    for m in movimientos:
+                        datos_inventario[nombre_apartado]["total"] += (m.debit - m.credit)
+                elif apartado.tipo == "acumulado_acreedor":
+                    movimientos = self.env["account.move.line"].search([("account_id","in", apartado.cuenta_ids.ids),("date",">=", fecha_inicio),("date","<=", fecha_fin)])
+                    for m in movimientos:
+                        datos_inventario[nombre_apartado]["total"] += (m.credit - m.debit)
+                # elif apartado.tipo == "balanza_final_deudor":
+                # elif apartado.tipo == "balanza_final_acreedor":
+                else:
+                    if apartado.codigo_py:
+                        movimiento = False
+    #                     logging.warning("calculado")
+    #                     logging.warning(datos_inventario)
+    #                     logging.warning(apartado.codigo_py)
+    #                     localdict = apartado.codigo_py
+    #                     datos_inventario[nombre_apartado]["total"] = safe_eval(apartado.codigo_py, json.loadslocaldict, mode="exec", nocopy=True)
+    # #localdict = {'base_amount': base_amount, 'price_unit': price_unit, 'quantity': quantity, 'product': product_sudo, 'partner': partner, 'company': company}
+    #safe_eval(self.python_compute, localdict, mode="exec", nocopy=True)
+                        
+        logging.warning(datos_inventario)
+        return datos_inventario
     
     def print_report_excel(self):
         for w in self:
@@ -50,6 +96,7 @@ class ReporteEstadoProyectado(models.TransientModel):
 
 
             gastos_indirectos = self.obtener_gastos_indirectos(w.fecha_inicio, w.fecha_fin, w.cuenta_analitica_id)
+            inventarios = self.obtener_inventario(w.fecha_inicio, w.fecha_fin)
             hoja = libro.add_worksheet('Estado proyectado')
             hoja.write(0,3,str(self.env.user.company_id.name))
             hoja.write(1,3,str(self.env.user.company_id.partner_id.contact_address_complete))
@@ -58,17 +105,25 @@ class ReporteEstadoProyectado(models.TransientModel):
             hoja.write(4,4,"02/28/2025")
 
 
-            hoja.write(6,0, 'Inventario Inicial de Producción en Proceso')
-            hoja.write(7,0, 'Inventario Inicial de Materia Prima')
-            hoja.write(8,0, 'compras de materia prima ')
-            hoja.write(9,0, 'Gastos de compra de materia prima')
-            hoja.write(10,0, 'disponible')
-            hoja.write(11,0, 'Inventario final de materia prima')
-            hoja.write(12,0, 'costo de la materia prima utilizada')
-            hoja.write(13,0, 'mano de obra directa')
-            hoja.write(14,0, 'costo primo')
+            # hoja.write(6,0, 'Inventario Inicial de Producción en Proceso')
+            # hoja.write(7,0, 'Inventario Inicial de Materia Prima')
+            # hoja.write(8,0, 'compras de materia prima ')
+            # hoja.write(9,0, 'Gastos de compra de materia prima')
+            # hoja.write(10,0, 'disponible')
+            # hoja.write(11,0, 'Inventario final de materia prima')
+            # hoja.write(12,0, 'costo de la materia prima utilizada')
+            # hoja.write(13,0, 'mano de obra directa')
+            # hoja.write(14,0, 'costo primo')
 
-            fila = 16
+            fila = 6
+            for apartado in inventarios:
+                if "nombre" in inventarios[apartado]:
+                    fila += 1
+                    logging.warning(inventarios[apartado])
+                    hoja.write(fila, 0, inventarios[apartado]["nombre"])
+                    hoja.write(fila, 1, inventarios[apartado]["total"])
+                    
+            fila += 2
             hoja.write(fila,0, 'Gastos Indirectos de Producción')
             for gasto in gastos_indirectos[0]:
                 fila += 1
